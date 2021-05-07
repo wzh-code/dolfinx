a0462cc4cc2fe8b5f91e4fd57bf14db3bd189f1a
Garth N. Wells <gnw20@cam.ac.uk>
2021-05-07 20:30:07 +0100

    Remove commented code

--- python/dolfinx/wrappers/array.h
+++ python/dolfinx/wrappers/array.h
@@ -11,10 +11,6 @@
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 
-// #define PY_ARRAY_UNIQUE_SYMBOL my_uniqe_array_api
-// #include <xtensor-python/pyarray.hpp>
-// #include <xtensor-python/pytensor.hpp>
-
 namespace py = pybind11;
 
 namespace dolfinx_wrappers
