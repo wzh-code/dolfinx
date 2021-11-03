96028824723e2e7db155d27bfea19f6392d3c266
Jack S. Hale <mail@jackhale.co.uk>
2021-11-03 13:28:09 +0100

    Use parmetis by default.

--- cpp/dolfinx/graph/partition.cpp
+++ cpp/dolfinx/graph/partition.cpp
@@ -23,8 +23,8 @@ graph::partition_graph(MPI_Comm comm, int nparts,
                        const AdjacencyList<std::int64_t>& local_graph,
                        std::int32_t num_ghost_nodes, bool ghosting)
 {
-  return graph::scotch::partitioner()(comm, nparts, local_graph,
-                                      num_ghost_nodes, ghosting);
+  return graph::parmetis::partitioner()(comm, nparts, local_graph,
+                                        num_ghost_nodes, ghosting);
 }
 //-----------------------------------------------------------------------------
 std::tuple<graph::AdjacencyList<std::int64_t>, std::vector<int>,
